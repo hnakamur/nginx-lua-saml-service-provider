7347e8d32dfb14ae249e92a941409695040ecebe
Hiroaki Nakamura <hnakamur@gmail.com>
2018-07-27 22:16:23 +0900

    Initial commit

---
+++ saml/service_provider/request.lua
@@ -0,0 +1,79 @@
+-- Copyright (C) by Hiroaki Nakamura (hnakamur)
+
+local zlib = require "ffi-zlib"
+local setmetatable = setmetatable
+
+local _M = { _VERSION = '0.1.0' }
+
+local mt = { __index = _M }
+
+function _M.new(self, config)
+    return setmetatable({
+        idp_dest_url = config.idp_dest_url,
+        sp_entity_id = config.sp_entity_id,
+        sp_saml_finish_url = config.sp_saml_finish_url,
+        request_id_generator = config.request_id_generator
+    }, mt)
+end
+
+function _M.redirect_to_idp_to_sign_in(self)
+    local req, err = self:create_compress_base64encode_request()
+    if err ~= nil then
+        return nil, err
+    end
+    local url = self.idp_dest_url .. "?" .. ngx.encode_args({SAMLRequest = req})
+    return ngx.redirect(url)
+end
+
+function _M.create_compress_base64encode_request(self)
+    local request_xml = self:create_request_xml()
+    local compressed, err = self:compress(request_xml)
+    if err ~= nil then
+        return nil, err
+    end
+
+    return ngx.encode_base64(compressed)
+end
+
+function _M.create_request_xml(self)
+    local request_id = self.request_id_generator()
+    local now = ngx.utctime()
+    local issue_instant = string.sub(now, 1, #"yyyy-mm-dd") .. "T" .. string.sub(now, -#"hh:mm:ss") .. "Z"
+
+    return string.format(
+        '<samlp:AuthnRequest xmlns:samlp="urn:oasis:names:tc:SAML:2.0:protocol" AssertionConsumerServiceURL="%s" Destination="%s" ID="%s" IssueInstant="%s" ProtocolBinding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST" Version="2.0"><saml:Issuer xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion">%s</saml:Issuer><samlp:NameIDPolicy AllowCreate="1"/></samlp:AuthnRequest>',
+        self.sp_saml_finish_url,
+        self.idp_dest_url,
+        request_id,
+        issue_instant,
+        self.sp_entity_id
+    )
+end
+
+function _M.compress(self, request_xml)
+    local i = 1
+    local input = function(bufsize)
+        if i > #request_xml then
+            return nil
+        end
+        local ret = string.sub(request_xml, i, i + bufsize - 1)
+        i = i + bufsize
+        return ret
+    end
+
+    local output_table = {}
+    local output = function(data)
+        table.insert(output_table, data)
+    end
+
+    local options = {windowBits = 15}
+    local ok, err = zlib.deflateGzip(input, output, nil, options)
+    if not ok then
+        return nil, string.format("failed to compress SAML request, err=%s", err)
+    end
+
+    local compressed = string.sub(table.concat(output_table, ''), 2 + 1, -(4 + 1))
+    return compressed, nil
+end
+
+return _M
---
+++ saml/service_provider/response.lua
@@ -0,0 +1,97 @@
+-- Copyright (C) by Hiroaki Nakamura (hnakamur)
+
+local slaxml = require 'slaxml'
+local setmetatable = setmetatable
+
+local _M = { _VERSION = '0.1.0' }
+
+local mt = { __index = _M }
+
+function _M.new(self, config)
+    return setmetatable({
+        xmlsec_command = config.xmlsec_command,
+        idp_cert_filename = config.idp_cert_filename
+    }, mt)
+end
+
+function _M.read_and_base64decode_response(self)
+    ngx.req.read_body()
+    local args, err = ngx.req.get_post_args()
+    if err ~= nil then
+       return nil, string.format("failed to get post args to read SAML response, err=%s", err)
+    end
+
+    return ngx.decode_base64(args.SAMLResponse)
+end
+
+function _M.verify_response(self, response_xml)
+    local tmpfilename = os.tmpname()
+    local file, err = io.open(tmpfilename, "w")
+    if err ~= nil then
+       return false, string.format("failed to open temporary file for writing SAML response, err=%s", err)
+    end
+    file:write(response_xml)
+    file:close()
+
+    local cmd = string.format("%s --verify --pubkey-cert-pem %s --id-attr:ID urn:oasis:names:tc:SAML:2.0:protocol:Response %s",
+        self.xmlsec_command, self.idp_cert_filename, tmpfilename)
+    local code = os.execute(cmd)
+    if code ~= 0 then
+       return false, string.format("failed to verify SAML response, exitcode=%d", code)
+    end
+
+    local ok, err = os.remove(tmpfilename)
+    if not ok then
+       return false, string.format("failed to delete SAML response tmpfile, filename=%s, err=%s", tmpfilename, err)
+    end
+    return true
+end
+
+function _M.take_attributes_from_response(self, response_xml)
+    local onAttributeElemStart = false
+    local inAttributeElem = false
+    local inAttributeValueElem = false
+    local attrs = {}
+    local attr_name
+
+    local handleStartElement = function(name, nsURI, nsPrefix)
+        if nsPrefix == "saml" and name == "Attribute" then
+            onAttributeElemStart = true
+            inAttributeElem = true
+        else
+            onAttributeElemStart = false
+        end
+        if nsPrefix == "saml" and name == "AttributeValue" then
+            inAttributeValueElem = true
+        end
+    end
+    local handleAttribute = function(name, value, nsURI, nsPrefix)
+        if onAttributeElemStart and name == "Name" then
+            attr_name = value
+        end
+    end
+    local handleCloesElement = function(name, nsURI)
+        if nsPrefix == "saml" and name == "Attribute" then
+            inAttributeElem = false
+        end
+        if nsPrefix == "saml" and name == "AttributeValue" then
+            inAttributeValueElem = false
+        end
+    end
+
+    local handleText = function(text)
+        if inAttributeValueElem then
+            attrs[attr_name] = text
+        end
+    end
+    local parser = slaxml:parser{
+        startElement = handleStartElement,
+        attribute = handleAttribute,
+        closeElement = handleCloseElement,
+        text = handleText
+    }
+    parser:parse(response_xml, {stripWhitespace=true})
+    return attrs
+end
+
+return _M
