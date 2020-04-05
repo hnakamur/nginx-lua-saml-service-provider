8c18449d63e118ac539e95a639356df4e8e42cf6
Hiroaki Nakamura <hnakamur@gmail.com>
2020-04-05 14:03:17 +0900

    Delete docker-compose based test files

--- test_files/client/test-and-verify.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-test-saml-login.sh | tee /tmp/actual-result.txt
-if cmp /tmp/actual-result.txt /tmp/expected-result.txt; then
-    echo "Test result matched, OK!"
-else
-    echo "Test result unmatch to expected-result.txt, here is diff:"
-    diff -u /tmp/actual-result.txt /tmp/expected-result.txt
-fi
